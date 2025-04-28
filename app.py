5f790a47f6c508e3c5a552ce1c35eef452279ba7
garotar <noob0094xd@yandex.ru>
2025-04-29 02:03:30 +0300

    [ADD] streamlit app

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.title("Прогнозирование доходностей и оптимизация портфеля")
+    st.sidebar.title("Параметры")
+
+
+if __name__ == "__main__":
+    main()
